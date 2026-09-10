7f6bcbfae478c5fe1b2421d1895a8a3a7c84d00b
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 11:19:52 +0530

    Update outputs.tf

--- security/outputs.tf
+++ security/outputs.tf
@@ -1,1 +1,19 @@
+output "kms_key_id" {
+  description = "ID of the security KMS key"
+  value       = aws_kms_key.main.key_id
+}
 
+output "kms_key_arn" {
+  description = "ARN of the security KMS key"
+  value       = aws_kms_key.main.arn
+}
+
+output "kms_alias" {
+  description = "Alias of the security KMS key"
+  value       = aws_kms_alias.main.name
+}
+
+output "security_audit_role_arn" {
+  description = "ARN of the security audit IAM role"
+  value       = aws_iam_role.security_audit.arn
+}
