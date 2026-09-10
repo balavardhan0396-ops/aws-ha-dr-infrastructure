969e1202d6e443f70d76ca6403f70316a4815689
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 11:13:00 +0530

    Update alb.tf

--- application/alb.tf
+++ application/alb.tf
@@ -1,1 +1,40 @@
 
+# ---------------------------------------------------------
+# Application Load Balancer
+# ---------------------------------------------------------
+
+resource "aws_lb" "app" {
+  name               = "${var.project_name}-${var.environment}-alb"
+  internal           = false
+  load_balancer_type = "application"
+
+  security_groups = [
+    data.terraform_remote_state.network.outputs.alb_security_group_id
+  ]
+
+  subnets = data.terraform_remote_state.network.outputs.public_subnet_ids
+
+  enable_deletion_protection = false
+
+  tags = {
+    Name = "${var.project_name}-alb"
+  }
+}
+
+
+# ---------------------------------------------------------
+# HTTP Listener
+# ---------------------------------------------------------
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.app.arn
+
+  port     = 80
+  protocol = "HTTP"
+
+  default_action {
+    type = "forward"
+
+    target_group_arn = aws_lb_target_group.app.arn
+  }
+}
