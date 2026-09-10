69b7a05719b7faa1f9ee92c29311d925ce6b1514
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 11:19:12 +0530

    Update kms.tf

--- security/kms.tf
+++ security/kms.tf
@@ -1,1 +1,35 @@
 
+resource "aws_kms_key" "main" {
+  description             = "KMS key for ${var.project_name} ${var.environment} encryption"
+  enable_key_rotation     = true
+  deletion_window_in_days = 7
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Sid    = "EnableIAMUserPermissions"
+        Effect = "Allow"
+
+        Principal = {
+          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+        }
+
+        Action   = "kms:*"
+        Resource = "*"
+      }
+    ]
+  })
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-kms-key"
+  }
+}
+
+resource "aws_kms_alias" "main" {
+  name          = "alias/${var.project_name}-${var.environment}"
+  target_key_id = aws_kms_key.main.key_id
+}
+
+data "aws_caller_identity" "current" {}
