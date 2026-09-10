6082860388d028dc986d2ddf4ae1af60a6631000
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 11:24:35 +0530

    Update provider.tf

--- monitoring/provider.tf
+++ monitoring/provider.tf
@@ -1,1 +1,13 @@
 
+provider "aws" {
+  region = var.aws_region
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Component   = "Monitoring"
+    }
+  }
+}
