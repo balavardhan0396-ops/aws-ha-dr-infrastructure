1e2611119ae88d60a949a6eba9902fd36affd2b7
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 11:12:19 +0530

    Update target-groups.tf

--- application/target-groups.tf
+++ application/target-groups.tf
@@ -1,1 +1,29 @@
+# ---------------------------------------------------------
+# Application Target Group
+# ---------------------------------------------------------
 
+resource "aws_lb_target_group" "app" {
+  name     = "${var.project_name}-${var.environment}-tg"
+  port     = var.app_port
+  protocol = "HTTP"
+
+  vpc_id = data.terraform_remote_state.network.outputs.vpc_id
+
+  target_type = "instance"
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 2
+    unhealthy_threshold = 3
+    interval            = 30
+    timeout             = 5
+    protocol            = "HTTP"
+    port                = "traffic-port"
+    path                = var.health_check_path
+    matcher             = "200"
+  }
+
+  tags = {
+    Name = "${var.project_name}-app-target-group"
+  }
+}
