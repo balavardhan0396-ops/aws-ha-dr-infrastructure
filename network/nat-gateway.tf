c89f61a20d8967f12d8d08c97eaffb50e360e959
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 10:24:17 +0530

    Update nat-gateway.tf

--- network/nat-gateway.tf
+++ network/nat-gateway.tf
@@ -1,1 +1,25 @@
 
+resource "aws_eip" "nat" {
+  count = length(var.availability_zones)
+
+  domain = "vpc"
+
+  tags = {
+    Name = "${var.project_name}-nat-eip-${count.index + 1}"
+  }
+}
+
+resource "aws_nat_gateway" "main" {
+  count = length(var.availability_zones)
+
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  depends_on = [
+    aws_internet_gateway.main
+  ]
+
+  tags = {
+    Name = "${var.project_name}-nat-${count.index + 1}"
+  }
+}
