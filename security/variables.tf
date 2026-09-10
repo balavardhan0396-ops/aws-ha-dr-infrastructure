898c05f0fa3e58193b29debf9dfa2ff757228a68
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 11:18:39 +0530

    Update variables.tf

--- security/variables.tf
+++ security/variables.tf
@@ -1,1 +1,17 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
 
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+  default     = "aws-ha-dr"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "lab"
+}
