19423567f4062b45c25f4d398690bd9767a6ac76
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 11:14:16 +0530

    Update autoscaling.tf

--- application/autoscaling.tf
+++ application/autoscaling.tf
@@ -1,1 +1,49 @@
+# ---------------------------------------------------------
+# Auto Scaling Group
+# ---------------------------------------------------------
 
+resource "aws_autoscaling_group" "app" {
+  name = "${var.project_name}-${var.environment}-asg"
+
+  min_size         = var.min_size
+  desired_capacity = var.desired_capacity
+  max_size         = var.max_size
+
+  vpc_zone_identifier = data.terraform_remote_state.network.outputs.app_subnet_ids
+
+  health_check_type         = "ELB"
+  health_check_grace_period = 120
+
+  target_group_arns = [
+    aws_lb_target_group.app.arn
+  ]
+
+  launch_template {
+    id      = aws_launch_template.app.id
+    version = "$Latest"
+  }
+
+  instance_refresh {
+    strategy = "Rolling"
+
+    preferences {
+      min_healthy_percentage = 50
+    }
+  }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.project_name}-app"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Tier"
+    value               = "Application"
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
