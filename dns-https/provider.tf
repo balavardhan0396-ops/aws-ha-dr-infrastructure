88d6eff1eace8010c6f1ba26ea9d8aa1503f2e52
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 11:35:25 +0530

    Update provider.tf

--- dns-https/provider.tf
+++ dns-https/provider.tf
@@ -1,1 +1,26 @@
+provider "aws" {
+  region = var.aws_region
 
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Component   = "DNS-HTTPS"
+    }
+  }
+}
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "Terraform"
+      Component   = "DNS-HTTPS"
+    }
+  }
+}
