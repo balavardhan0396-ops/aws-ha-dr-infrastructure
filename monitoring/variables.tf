97514eb2bf11cfd7054315025b914b210201826a
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-10 11:25:13 +0530

    Update variables.tf

--- monitoring/variables.tf
+++ monitoring/variables.tf
@@ -1,1 +1,22 @@
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "eu-north-1"
+}
 
+variable "project_name" {
+  description = "Project name used for resource naming"
+  type        = string
+  default     = "aws-ha-dr"
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+  default     = "lab"
+}
+
+variable "notification_email" {
+  description = "Email address for CloudWatch alarm notifications"
+  type        = string
+}
