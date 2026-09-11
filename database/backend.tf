92b511351e3095c86d64cb5b65f84a5fdbbf74c7
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-11 12:01:19 +0530

    Update backend.tf

--- database/backend.tf
+++ database/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "aws-ha-dr-terraform-state"
-    key          = "02-database/terraform.tfstate"
+    key          = "database/terraform.tfstate"
     region       = "eu-north-1"
     use_lockfile = true
   }
