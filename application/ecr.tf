c3f74b265cd0de85e1d1cb74380eb957344bf6e5
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-11 12:11:21 +0530

    Create ecr.tf

---
+++ application/ecr.tf
@@ -0,0 +1,44 @@
+# ---------------------------------------------------------
+# Amazon ECR Repository
+# ---------------------------------------------------------
+
+resource "aws_ecr_repository" "app" {
+  name                 = "${var.project_name}-${var.environment}-app"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-${var.environment}-app-ecr"
+  }
+}
+
+# ---------------------------------------------------------
+# ECR Lifecycle Policy
+# ---------------------------------------------------------
+
+resource "aws_ecr_lifecycle_policy" "app" {
+  repository = aws_ecr_repository.app.name
+
+  policy = jsonencode({
+    rules = [
+      {
+        rulePriority = 1
+
+        description = "Keep the latest 10 images"
+
+        selection = {
+          tagStatus   = "any"
+          countType   = "imageCountMoreThan"
+          countNumber = 10
+        }
+
+        action = {
+          type = "expire"
+        }
+      }
+    ]
+  })
+}
