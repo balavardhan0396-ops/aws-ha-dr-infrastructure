e59848ac68672ee100ded43de8fa0b11e63ab38c
balavardhan0396-ops <balavardhan0396@gmail.com>
2026-09-14 12:44:11 +0530

    Update cloudwatch.tf

--- monitoring/cloudwatch.tf
+++ monitoring/cloudwatch.tf
@@ -1,7 +1,3 @@
-# ---------------------------------------------------------
-# Remote State: Network
-# ---------------------------------------------------------
-
 data "terraform_remote_state" "network" {
   backend = "s3"
 
@@ -11,10 +7,6 @@ data "terraform_remote_state" "network" {
     region = var.aws_region
   }
 }
-
-# ---------------------------------------------------------
-# Remote State: Database
-# ---------------------------------------------------------
 
 data "terraform_remote_state" "database" {
   backend = "s3"
@@ -26,10 +18,6 @@ data "terraform_remote_state" "database" {
   }
 }
 
-# ---------------------------------------------------------
-# Remote State: Application
-# ---------------------------------------------------------
-
 data "terraform_remote_state" "application" {
   backend = "s3"
 
@@ -38,4 +26,189 @@ data "terraform_remote_state" "application" {
     key    = "application/terraform.tfstate"
     region = var.aws_region
   }
+}
+
+resource "aws_cloudwatch_dashboard" "main" {
+  dashboard_name = "${var.project_name}-${var.environment}-monitoring"
+
+  dashboard_body = jsonencode({
+    widgets = [
+      {
+        type   = "metric"
+        x      = 0
+        y      = 0
+        width  = 12
+        height = 6
+
+        properties = {
+          title  = "Application EC2 CPU Utilization"
+          region = var.aws_region
+          view   = "timeSeries"
+          period = 300
+          stat   = "Average"
+
+          metrics = [
+            [
+              "AWS/EC2",
+              "CPUUtilization",
+              "AutoScalingGroupName",
+              data.terraform_remote_state.application.outputs.autoscaling_group_name
+            ]
+          ]
+        }
+      },
+
+      {
+        type   = "metric"
+        x      = 12
+        y      = 0
+        width  = 12
+        height = 6
+
+        properties = {
+          title  = "Application Load Balancer Requests"
+          region = var.aws_region
+          view   = "timeSeries"
+          period = 300
+          stat   = "Sum"
+
+          metrics = [
+            [
+              "AWS/ApplicationELB",
+              "RequestCount",
+              "LoadBalancer",
+              data.terraform_remote_state.application.outputs.alb_arn_suffix
+            ]
+          ]
+        }
+      },
+
+      {
+        type   = "metric"
+        x      = 0
+        y      = 6
+        width  = 12
+        height = 6
+
+        properties = {
+          title  = "ALB HTTP 5XX Errors"
+          region = var.aws_region
+          view   = "timeSeries"
+          period = 300
+          stat   = "Sum"
+
+          metrics = [
+            [
+              "AWS/ApplicationELB",
+              "HTTPCode_ELB_5XX_Count",
+              "LoadBalancer",
+              data.terraform_remote_state.application.outputs.alb_arn_suffix
+            ]
+          ]
+        }
+      },
+
+      {
+        type   = "metric"
+        x      = 12
+        y      = 6
+        width  = 12
+        height = 6
+
+        properties = {
+          title  = "ALB Unhealthy Application Instances"
+          region = var.aws_region
+          view   = "timeSeries"
+          period = 300
+          stat   = "Maximum"
+
+          metrics = [
+            [
+              "AWS/ApplicationELB",
+              "UnHealthyHostCount",
+              "LoadBalancer",
+              data.terraform_remote_state.application.outputs.alb_arn_suffix,
+              "TargetGroup",
+              data.terraform_remote_state.application.outputs.target_group_arn_suffix
+            ]
+          ]
+        }
+      },
+
+      {
+        type   = "metric"
+        x      = 0
+        y      = 12
+        width  = 12
+        height = 6
+
+        properties = {
+          title  = "RDS CPU Utilization"
+          region = var.aws_region
+          view   = "timeSeries"
+          period = 300
+          stat   = "Average"
+
+          metrics = [
+            [
+              "AWS/RDS",
+              "CPUUtilization",
+              "DBInstanceIdentifier",
+              data.terraform_remote_state.database.outputs.rds_instance_id
+            ]
+          ]
+        }
+      },
+
+      {
+        type   = "metric"
+        x      = 12
+        y      = 12
+        width  = 12
+        height = 6
+
+        properties = {
+          title  = "RDS Database Connections"
+          region = var.aws_region
+          view   = "timeSeries"
+          period = 300
+          stat   = "Average"
+
+          metrics = [
+            [
+              "AWS/RDS",
+              "DatabaseConnections",
+              "DBInstanceIdentifier",
+              data.terraform_remote_state.database.outputs.rds_instance_id
+            ]
+          ]
+        }
+      },
+
+      {
+        type   = "metric"
+        x      = 0
+        y      = 18
+        width  = 12
+        height = 6
+
+        properties = {
+          title  = "RDS Free Storage Space"
+          region = var.aws_region
+          view   = "timeSeries"
+          period = 300
+          stat   = "Average"
+
+          metrics = [
+            [
+              "AWS/RDS",
+              "FreeStorageSpace",
+              "DBInstanceIdentifier",
+              data.terraform_remote_state.database.outputs.rds_instance_id
+            ]
+          ]
+        }
+      }
+    ]
+  })
 }
